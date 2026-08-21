50fe1414ddd1915ce347b78854abd35865a7f407
Juan MG <jeithree@users.noreply.github.com>
2026-08-21 14:00:04 -0500

    ci(pm2): change script path

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,14 +1,14 @@
 export default {
-	apps: [
-		{
-			name: 'api',
-			cwd: '/home/user-api/htdocs/api.jeitdev.com/api',
-			script: 'npm',
-			args: 'start',
+    apps: [
+        {
+            name: 'api',
+            cwd: '/home/user-api/htdocs/api.jeitdev.com/api',
+            script: '/home/user-api/.nvm/versions/node/v24.19.0/bin/npm',
+            args: 'start',
             interpreter: 'none',
-			env: {
-				NODE_ENV: 'production',
-			},
-		},
-	],
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
 };
