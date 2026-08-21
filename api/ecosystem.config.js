2b34ba079f100765c3ae83a4df1610f64f02d4b0
Juan MG <jeithree@users.noreply.github.com>
2026-08-21 14:09:05 -0500

    ci(pm2): change script path to use the real script instead of calling it with npm

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -3,9 +3,7 @@ export default {
         {
             name: 'api',
             cwd: '/home/user-api/htdocs/api.jeitdev.com/api',
-            script: '/home/user-api/.nvm/versions/node/v24.19.0/bin/npm',
-            args: 'start',
-            interpreter: 'none',
+            script: 'node --env-file=.env src/app/server.ts',
             env: {
                 NODE_ENV: 'production',
             },
