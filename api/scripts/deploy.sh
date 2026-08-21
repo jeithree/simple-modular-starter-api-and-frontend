340d1fa966eff399a83f4be7a86c00cf61522c56
Juan MG <jeithree@users.noreply.github.com>
2026-08-21 17:08:14 -0500

    ci: improve deploy bash scripts to show logs

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -15,15 +15,16 @@ npx prisma migrate deploy
 
 pm2 reload ecosystem.config.cjs --update-env
 
-echo "Checking API health..."
+# TODO: need to implement api healt to be used here
+# echo "Checking API health..."
 
-curl --fail --silent --show-error \
-  --connect-timeout 2 \
-  --max-time 5 \
-  --retry 30 \
-  --retry-delay 2 \
-  --retry-connrefused \
-  http://127.0.0.1:5000
+# curl --fail --silent --show-error \
+#   --connect-timeout 2 \
+#   --max-time 5 \
+#   --retry 30 \
+#   --retry-delay 2 \
+#   --retry-connrefused \
+#   http://127.0.0.1:5000/api/v1/health
 
-echo "API health check passed."
+# echo "API health check passed."
 echo "API deployed successfully: $COMMIT"
