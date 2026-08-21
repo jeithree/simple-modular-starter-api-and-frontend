b4b6e090702255f6374bb222f811023f216f2b22
Juan MG <jeithree@users.noreply.github.com>
2026-08-21 14:18:16 -0500

    ci(pm2): change from .js to .cjs to fix pm2 errors

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -1,12 +1,10 @@
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
 	apps: [
 		{
 			name: 'frontend',
 			cwd: '/home/user-frontend/htdocs/app.jeitdev.com/frontend',
-			script: 'npm',
-			args: 'start',
-            interpreter: 'none',
+			script: 'node_modules/next/dist/bin/next',
+            args: 'start -p 3000',
 			env: {
 				NODE_ENV: 'production',
 			},
