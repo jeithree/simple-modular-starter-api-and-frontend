c29b4ce14e88e955e88f9e3cd163d41a792f0729
Juan MG <jeithree@users.noreply.github.com>
2026-08-21 15:43:58 -0500

    fix(ci): move git pull to github action and remove from bash scripts

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-
 set -e
 
-echo "HOME=$HOME"
-echo "NVM_DIR=$NVM_DIR"
-
 export NVM_DIR="$HOME/.nvm"
-
-echo "NVM_DIR=$NVM_DIR"
-
 source "$NVM_DIR/nvm.sh"
 
-echo "PATH=$PATH"
-echo "npm=$(command -v npm)"
-echo "node=$(command -v node)"
-echo "pm2=$(command -v pm2)"
+cd /home/user-api/htdocs/api.jeitdev.com/api
 
-exit 0
+npm ci
+npx prisma migrate deploy
+
+pm2 reload ecosystem.config.cjs --update-env
--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -4,11 +4,7 @@ set -e
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 
-cd /home/user-frontend/htdocs/app.jeitdev.com
-
-git pull origin main
-
-cd frontend
+cd /home/user-frontend/htdocs/app.jeitdev.com/frontend
 
 npm ci
 npm run build
