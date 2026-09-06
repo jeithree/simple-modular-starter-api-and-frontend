b9166fb617bcdb22d3cb0978286f072c1e4f685d
Juan MG <jeithree@users.noreply.github.com>
2026-09-06 10:25:44 -0500

    fix(ecosystem): update script command to use node_args for environment variables

--- api/ecosystem.config.cjs
+++ api/ecosystem.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
-    apps: [
-        {
-            name: 'api',
-            cwd: '/home/user-api/htdocs/api.jeitdev.com/api',
-            script: 'node --env-file=.env src/app/server.ts',
-            env: {
-                NODE_ENV: 'production',
-            },
-        },
-    ],
+	apps: [
+		{
+			name: 'api',
+			cwd: '/home/user-api/htdocs/api.jeitdev.com/api',
+			script: 'src/app/server.ts',
+			node_args: '--env-file=.env',
+			env: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
 };
