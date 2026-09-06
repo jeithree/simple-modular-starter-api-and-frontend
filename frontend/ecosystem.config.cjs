5132323d903aa7d187046cd91071bc83a522b460
Juan MG <jeithree@users.noreply.github.com>
2026-09-06 11:19:12 -0500

    fix(ecosystem): remove unnecessary cwd property from frontend configuration

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -2,7 +2,6 @@ module.exports = {
 	apps: [
 		{
 			name: 'frontend',
-			cwd: '/home/user-frontend/htdocs/app.jeitdev.com/frontend',
 			script: 'node_modules/next/dist/bin/next',
             args: 'start -p 3000',
 			env: {
